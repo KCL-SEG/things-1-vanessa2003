b719de03cb33502138bbabcdcf32f715d575c474
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-10-30 15:04:38 +0000

    parameter passed changed from AbstractUser to models.Model

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Thing(AbstractUser):
+class Thing(models.Model):
     description = models.TextField()
     quantity = models.SmallIntegerField()
     name = models.TextField()
