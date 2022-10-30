c09f8fbd45cb36a874a3fc1462512ee19cde386e
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-10-30 15:11:51 +0000

    changed models.Model back to abstractUser

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Thing(models.Model):
+class Thing(AbstractUser):
     description = models.TextField()
     quantity = models.SmallIntegerField()
     name = models.TextField()
