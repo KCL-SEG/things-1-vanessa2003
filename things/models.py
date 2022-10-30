b896a883d068c98a2e72bfa0da6574e9f2b0bb8e
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-10-30 16:54:14 +0000

    add constraints

--- things/models.py
+++ things/models.py
@@ -1,14 +1,17 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
     REQUIRED_FIELDS = ('thing',)
 
 
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.SmallIntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.SmallIntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
     
     
 
